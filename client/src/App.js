a620f5970dbca8bc263f33d4f74a5387be591ebc
hadil <hihijazi@gmail.com>
2024-03-23 19:05:22 -0400

    save all changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 // App.js
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 
-function App() {
-  return <Home />;
+import CoursesForm from './components/Courses-section/CoursesForm';
+import ChatBot from './components/Chat-bot/Chatbot'; 
+import InstructorLogin from './components/InstructorLogin';
+import StudentLogin from './components/StudentLogin'; 
+import Signup from './components/Signup.js';
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<CoursesForm />} />
+        <Route path="/chatbot" element={<ChatBot />} />
+        <Route path="/instructorlogin" element={<InstructorLogin />} />
+        <Route path="/studentlogin" element={<StudentLogin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;
+export default App
